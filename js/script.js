83f48e443a63caf990c9dd684303d5dd5a3bd610
rafaelmjardim <rafaelmjardim@gotmail.com>
2022-06-18 19:32:25 -0300

    Nav Responsivo inicio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    var open = document.getElementsByClassName('menu-mobile')[0];
+
+    open.style.width = '100%';
+}
+function closeNav() {
+    var close = document.getElementsByClassName('menu-mobile')[0];
+
+    close.style.width = '0';
+}
