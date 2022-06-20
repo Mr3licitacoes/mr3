64239ead0b009a51c2dc78d51e62be0cfdd0f89f
rafaelmjardim <rafaelmjardim@gotmail.com>
2022-06-20 20:01:29 -0300

    menu e botão agendar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 function openNav() {
     var open = document.getElementsByClassName('menu-mobile')[0];
+    var openBtn = document.getElementById('open-btn');
 
     open.style.width = '100%';
+    openBtn.style.display = 'none';
 }
 function closeNav() {
     var close = document.getElementsByClassName('menu-mobile')[0];
+    var openBtn = document.getElementById('open-btn');
 
     close.style.width = '0';
+    openBtn.style.display = 'block';
 }
